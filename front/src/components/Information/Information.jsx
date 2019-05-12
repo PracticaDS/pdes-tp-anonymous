2474e9aa449e876d31d28a5e00ee63974a8390ff
juanmottesi <juannpm159@gmail.com>
2019-05-11 23:24:15 -0300

    Fix lint script and update code

--- front/src/components/Information/Information.jsx
+++ front/src/components/Information/Information.jsx
@@ -1,19 +1,13 @@
 import React from 'react';
-import './Information.css'
+import './Information.css';
 
-class Information extends React.Component {
-    render() {
-        return (
-            <div className="information-container">
-                <h2>Detalles</h2>
-                <div className="information-details">
-                    <div className="info name">Starter</div>
-                    <div className="info cost">Costo: <span>$800</span></div>
-                    <div className="info frequency">Frecuencia: <span>1/s</span></div>
-                </div>
-            </div>
-        );
-    }
-}
-
-export default Information;
+export default () => (
+  <div className="information-container">
+    <h2>Detalles</h2>
+    <div className="information-details">
+      <div className="info name">Starter</div>
+      <div className="info cost">Costo: <span>$800</span></div>
+      <div className="info frequency">Frecuencia: <span>1/s</span></div>
+    </div>
+  </div>
+);
