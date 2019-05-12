0371b422fcc4b0d7f9d2deb4533357f0cd440a40
juanmottesi <juannpm159@gmail.com>
2019-05-12 10:45:07 -0300

    fixed and added tests

--- front/src/components/__tests__/WorkingArea/EngineGrid/EngineGrid.spec.jsx
+++ front/src/components/__tests__/WorkingArea/EngineGrid/EngineGrid.spec.jsx
@@ -1,28 +1,67 @@
+import configureStore from 'redux-mock-store';
 import React from 'react';
-import { createStore } from 'redux';
+import { Provider } from 'react-redux';
 import { mount } from 'enzyme';
 
 import EngineGrid from '../../../WorkingArea/EngineGrid/EngineGrid';
-import reducers from '../../../../reducers/toolboxReducer';
 
-const store = createStore(reducers);
+const mockStore = configureStore([]);
+
+function mockState() {
+  return {
+    machines: [
+      {
+        position: { x: 0, y: 0 },
+        type: 'EMPTY',
+      },
+      {
+        position: { x: 0, y: 1 },
+        type: 'EMPTY',
+      },
+      {
+        position: { x: 1, y: 0 },
+        type: 'EMPTY',
+      },
+      {
+        position: { x: 1, y: 1 },
+        type: 'EMPTY',
+      },
+    ],
+    width: 2,
+  };
+}
 
 describe('Engine Grid component', () => {
   it('The grid is represented with a table', () => {
-    const grid = mount(<EngineGrid store={store} />);
+    const store = mockStore(mockState);
+    const grid = mount(
+      <Provider store={store}>
+        <EngineGrid />
+      </Provider>,
+    );
     expect(grid.find('table').length).toEqual(1);
   });
   it('The grid is created with a size of x per y', () => {
-    const width = 6;
-    const height = 5;
-    const grid = mount(<EngineGrid store={store} />);
+    const width = 2;
+    const height = 2;
+    const store = mockStore(mockState);
+    const grid = mount(
+      <Provider store={store}>
+        <EngineGrid />
+      </Provider>,
+    );
     expect(grid.find('tr').length).toEqual(height);
     expect(grid.find('td').length).toEqual(width * height);
   });
   it('The grid init empty', () => {
-    const width = 4;
-    const height = 5;
-    const grid = mount(<EngineGrid store={store} />);
+    const width = 2;
+    const height = 2;
+    const store = mockStore(mockState);
+    const grid = mount(
+      <Provider store={store}>
+        <EngineGrid />
+      </Provider>,
+    );
     expect(grid.find('.empty').length).toEqual(width * height);
   });
 });
