ef91aae300fc153f7178627235a523f410f8f4a4
juanmottesi <juannpm159@gmail.com>
2019-05-13 00:07:55 -0300

    added tick

---
+++ front/src/components/__tests__/WorkingArea/WorkingArea.spec.jsx
@@ -0,0 +1,33 @@
+import configureStore from 'redux-mock-store';
+import React from 'react';
+import { Provider } from 'react-redux';
+import { mount } from 'enzyme';
+
+import WorkingArea from '../../WorkingArea/WorkingArea';
+
+const mockStore = configureStore([]);
+
+describe('Working Area', () => {
+  it('should call init', () => {
+    const store = mockStore({});
+    mount(
+      <Provider store={store}>
+        <WorkingArea />
+      </Provider>,
+    );
+    const action = store.getActions()[0];
+    expect(action).toEqual({ type: 'INIT', payload: { width: 4, height: 4 } });
+  });
+  it('should call init and tick', (success) => {
+    const store = mockStore({});
+    mount(
+      <Provider store={store}>
+        <WorkingArea />
+      </Provider>,
+    );
+    setTimeout(() => {
+      expect(store.getActions()).toEqual([{ type: 'INIT', payload: { width: 4, height: 4 } }, { type: 'TICK' }]);
+      success();
+    }, 3005);
+  });
+});
