0508ee31eddbca8b8685dcdc0c68142e311eaca0
Leandro <leandro.jdl@gmail.com>
2019-05-29 22:35:08 -0300

    Fixing

--- .travis/deploy.sh
+++ .travis/deploy.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
 
-echo $TRAVIS_BRANCH
 if [ "${TRAVIS_BRANCH}" = "master" ] && [ "${TRAVIS_PULL_REQUEST}" = "false" ]; then
     echo "162.243.162.94 ecdsa-sha2-nistp256 $SERVER_KEY" >> ~/.ssh/known_hosts
     ssh forge@162.243.162.94 '/home/forge/pdes-tp-anonymous-deploy.sh'
