38e3570a425c7e9aa470c17b2f2e2340154880e8
Leandro <leandro.jdl@gmail.com>
2019-05-29 22:50:18 -0300

    min change para ver el auto deploy

--- back/src/index.js
+++ back/src/index.js
@@ -4,7 +4,7 @@ const app = express();
 const port = 8080;
 
 app.get('/', (req, res) => {
-  res.json({ message: 'Ok' })
+  res.json({ message: 'Ok, auto deploy working' })
     .status(200);
 });
 
