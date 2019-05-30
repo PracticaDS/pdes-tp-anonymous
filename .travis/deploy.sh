9fec0f0f905d41c0015db5b0e273dbe924f9198e
Leandro <leandro.jdl@gmail.com>
2019-05-29 21:32:50 -0300

    Agrego comandos para deploy automático a DO

---
+++ .travis/deploy.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+if [ $TRAVIS_BRANCH == 'master' ] ; then
+    echo "162.243.162.94 ecdsa-sha2-nistp256 $SERVER_KEY" >> ~/.ssh/known_hosts
+    ssh forge@162.243.162.94 '/home/forge/pdes-tp-anonymous-deploy.sh'
+else
+    echo "Not deploying, since this branch isn't master."
+fi
