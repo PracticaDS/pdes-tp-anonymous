30b3e77c3d78dadf7f44a69fb568ee5257faeaa0
Leandro <leandro.jdl@gmail.com>
2019-06-03 22:11:38 -0300

    Agrego conexión con mongo así ya queda y pruebo travis

--- back/src/app/index.js
+++ back/src/app/index.js
@@ -1,8 +1,18 @@
 const express = require('express');
+const mongoose = require('mongoose');
 // const User = require('../models/user');
 
-const app = express();
+const domain = 'localhost';
 const port = 8080;
+const portDB = 27017;
+const app = express();
+
+// Mongoose connection
+const db = mongoose.connection;
+mongoose.connect(`mongodb://${domain}:${portDB}/prueba`, { useNewUrlParser: true });
+db.on('error', console.error.bind(console, 'MongoDB connection error:'));
+db.once('open', () => console.info('MongoDB Connection OK'));
+
 
 app.get('/', (req, res) => {
   res
@@ -11,7 +21,7 @@ app.get('/', (req, res) => {
 });
 
 app.listen(port, () => {
-  console.info(`Se inicio la aplicación en: http://localhost:${port}`);
+  console.info(`Se inicio la aplicación en: http://${domain}:${port}`);
 });
 
 module.exports = app;
