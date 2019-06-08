d6e3bb740156fcead4633461f8d0e16eeaec4813
Facundo Khalil <facundokhalil@gmail.com>
2019-06-08 18:20:08 -0300

    Se creo la pantalla de login

--- front/src/components/Login/Login.jsx
+++ front/src/components/Login/Login.jsx
@@ -1,5 +1,18 @@
 import React from 'react';
+import './Login.css';
 
-const Login = () => <h1>Login papa</h1>;
+const Login = () => (
+  <div id="LoginForm">
+    <div className="row header-container">
+      <h1>Revolución Industrial</h1>
+    </div>
+
+    <div className="login-panel row col-4 offset-md-4">
+      <h4 className="row">Ingreso al juego</h4>
+      <input type="text" className="row form-control" id="user" placeholder="Ingrese su usuario" />
+      <button type="submit" className="row btn btn-secondary">Ingresar</button>
+    </div>
+  </div>
+);
 
 export default Login;
