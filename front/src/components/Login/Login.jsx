febfed986229bac23d91a1a3ff151ac4e465aeff
Facundo Khalil <facundokhalil@gmail.com>
2019-06-09 13:56:21 -0300

    Se termino la pantalla de login

--- front/src/components/Login/Login.jsx
+++ front/src/components/Login/Login.jsx
@@ -1,12 +1,14 @@
 import React from 'react';
-import axios from 'axios';
-import { ValidatorForm, TextValidator } from 'react-material-ui-form-validator';
 import './Login.css';
+import axios from 'axios';
 
 export default class Login extends React.Component {
   constructor(props) {
     super(props);
-    this.state = { username: '' };
+    this.state = {
+      username: '',
+      usernameError: '',
+    };
     this.handleChange = this.handleChange.bind(this);
     this.handleSubmit = this.handleSubmit.bind(this);
   }
@@ -16,12 +18,24 @@ export default class Login extends React.Component {
   }
 
   handleSubmit(event) {
-    axios.post('http://localhost:8080/users', { username: 'Pepe' })
-      .then((res) => {
-        console.info(res.data);
-      })
-      .catch(error => console.info(error));
     event.preventDefault();
+    const isValidate = this.validate();
+    if (isValidate) {
+      axios.post('http://localhost:8080/users', { username: this.state.username })
+        .then((res) => {
+          console.info(res.data);
+        })
+        .catch(error => console.info(error));
+    }
+  }
+
+  validate() {
+    if (this.state.username.length === 0) {
+      this.setState({ usernameError: 'Debe completar este campo' });
+      return false;
+    }
+
+    return true;
   }
 
   render() {
@@ -30,19 +44,23 @@ export default class Login extends React.Component {
         <div className="row header-container">
           <h1>Revolución Industrial</h1>
         </div>
-        <ValidatorForm className="login-panel row col-4 offset-md-4" onSubmit={this.handleSubmit}>
+        <form
+          className="login-panel row col-4 offset-md-4"
+          onSubmit={this.handleSubmit}
+          noValidate
+        >
           <h4 className="row">Ingreso al juego</h4>
-          <TextValidator
+          <input
             className="row form-control"
             type="text"
             placeholder="Ingrese su usuario"
             value={this.state.username}
             onChange={this.handleChange}
-            validators={['required']}
-            errorMessages={['El campo no puede estar vacio']}
+            required
           />
-          <button type="submit" className="row btn btn-secondary">Ingresar</button>
-        </ValidatorForm>
+          <div className="row col-12" style={{ color: 'red', fontSize: 13 }}>{this.state.usernameError}</div>
+          <button type="submit" className="row col-5 btn btn-secondary">Ingresar</button>
+        </form>
       </div>
     );
   }
