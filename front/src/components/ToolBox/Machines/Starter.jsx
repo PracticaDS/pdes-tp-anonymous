53155cc92fa9509be8024bb8e03f7d724b80fee0
juanmottesi <juannpm159@gmail.com>
2019-06-09 00:18:33 -0300

    added dropdown with recipes

--- front/src/components/ToolBox/Machines/Starter.jsx
+++ front/src/components/ToolBox/Machines/Starter.jsx
@@ -21,9 +21,10 @@ const StarterToolbox = (props) => {
       <div className="dropdown-menu" aria-labelledby="dropdownMenuLink">
         <a className="dropdown-item" href="#1" onClick={() => props.setCurrentAction('STARTER', material('GOLD'))}>Gold</a>
         <a className="dropdown-item" href="#2" onClick={() => props.setCurrentAction('STARTER', material('COPPER'))}>Copper</a>
-        <a className="dropdown-item" href="#2" onClick={() => props.setCurrentAction('STARTER', material('ALUMINUM'))}>Aluminum</a>
-        <a className="dropdown-item" href="#2" onClick={() => props.setCurrentAction('STARTER', material('COAL'))}>Coal</a>
-        <a className="dropdown-item" href="#2" onClick={() => props.setCurrentAction('STARTER', material('IRON'))}>Iron</a>
+        <a className="dropdown-item" href="#3" onClick={() => props.setCurrentAction('STARTER', material('ALUMINUM'))}>Aluminum</a>
+        <a className="dropdown-item" href="#4" onClick={() => props.setCurrentAction('STARTER', material('COAL'))}>Coal</a>
+        <a className="dropdown-item" href="#5" onClick={() => props.setCurrentAction('STARTER', material('IRON'))}>Iron</a>
+        <a className="dropdown-item" href="#6" onClick={() => props.setCurrentAction('STARTER', material('TIN'))}>Tin</a>
       </div>
     </div>
   );
