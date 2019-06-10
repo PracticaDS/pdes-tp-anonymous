9fb4ed326676f64ccc441290fe6535c7e4afb9b8
Facundo Khalil <facundokhalil@gmail.com>
2019-06-10 09:12:04 -0300

    Se saco el Routes y se dejo todo en app. Tambien se configuro el router para que funcione con el github pages

--- front/src/app/App.jsx
+++ front/src/app/App.jsx
@@ -1,10 +1,19 @@
 import './App.css';
 import React, { Component } from 'react';
-import Routes from './Routes';
+import { BrowserRouter as HashRouter, Route } from 'react-router-dom';
+import Login from '../components/User/Login';
+import Game from '../components/Game/Game';
+import UserProfile from '../components/User/UserProfile';
 
 class App extends Component {
   render() {
-    return <Routes />;
+    return (
+      <HashRouter basename="/">
+        <Route exact path="/" component={Login} />
+        <Route exact path="/:user" component={UserProfile} />
+        <Route exact path="/:user/:gameId" component={Game} />
+      </HashRouter>
+    );
   }
 }
 
