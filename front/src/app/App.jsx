0a73cf780969b3d81f0425d4e275771b08c1c80a
Facundo Khalil <facundokhalil@gmail.com>
2019-06-10 10:06:26 -0300

    Otro cambio en rutas para gh pages

--- front/src/app/App.jsx
+++ front/src/app/App.jsx
@@ -1,6 +1,6 @@
 import './App.css';
 import React, { Component } from 'react';
-import { BrowserRouter as HashRouter, Route } from 'react-router-dom';
+import { BrowserRouter as Router, Route } from 'react-router-dom';
 import Login from '../components/User/Login';
 import Game from '../components/Game/Game';
 import UserProfile from '../components/User/UserProfile';
@@ -8,11 +8,11 @@ import UserProfile from '../components/User/UserProfile';
 class App extends Component {
   render() {
     return (
-      <HashRouter basename="/">
+      <Router basename="/pdes-tp-anonymous">
         <Route exact path="/" component={Login} />
         <Route exact path="/:user" component={UserProfile} />
         <Route exact path="/:user/:gameId" component={Game} />
-      </HashRouter>
+      </Router>
     );
   }
 }
