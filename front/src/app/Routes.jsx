95f8bd0cdb8731b9b1f37b271297a754a1a1f60b
Facundo Khalil <facundokhalil@gmail.com>
2019-06-09 21:46:33 -0300

    Se creo la pantalla del perfil del usuario

--- front/src/app/Routes.jsx
+++ front/src/app/Routes.jsx
@@ -2,10 +2,12 @@ import React from 'react';
 import { BrowserRouter as Router, Route } from 'react-router-dom';
 import Login from '../components/Login/Login';
 import Game from '../components/Game/Game';
+import UserProfile from '../components/Login/UserProfile';
 
 const Routes = () => (
   <Router>
     <Route exact path="/" component={Login} />
+    <Route exact path="/:user/games" component={UserProfile} />
     <Route exact path="/game" component={Game} />
   </Router>
 );
