bd0844d86f26551c415cb5fa75d70bbd1fdaad25
juanmottesi <juannpm159@gmail.com>
2019-06-11 17:57:19 -0300

    Fix lint

--- front/src/app/Routes.jsx
+++ front/src/app/Routes.jsx
@@ -1,8 +1,8 @@
 import React from 'react';
 import { BrowserRouter as Router, Route } from 'react-router-dom';
-import Login from '../components/Login/Login';
+import Login from '../components/User/Login';
 import Game from '../components/Game/Game';
-import UserProfile from '../components/Login/UserProfile';
+import UserProfile from '../components/User/UserProfile';
 
 const Routes = () => (
   <Router>
