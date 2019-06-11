32519651e839958a46eea0534231709de9b618fb
Facundo Khalil <facundokhalil@gmail.com>
2019-06-11 15:52:35 -0300

    tests medios pelo de la vista. Falta mockear service y testear la logica

--- front/src/components/__tests__/WorkingArea/WorkingArea.spec.jsx
+++ front/src/components/__tests__/WorkingArea/WorkingArea.spec.jsx
@@ -1,42 +1,25 @@
-/* import configureStore from 'redux-mock-store';
+import configureStore from 'redux-mock-store';
 import React from 'react';
 import { Provider } from 'react-redux';
-import { shallow } from 'enzyme';
-
+import { mount } from 'enzyme';
 import WorkingArea from '../../WorkingArea/WorkingArea';
 
 const mockStore = configureStore([]);
 const store = mockStore({ currentAction: { action: null } });
-*/
+const workingArea = mount(
+  <Provider store={store}>
+    <WorkingArea />
+  </Provider>,
+);
 
 describe('Working Area', () => {
-  it('basic test', () => {
-    expect(true).toEqual(true);
+  it('Working area contains a working area container', () => {
+    expect(workingArea.find('div').at(0).hasClass('working-area-container')).toEqual(true);
   });
-  /* it('should call tick', () => {
-    const store = mockStore({ currentAction: { action: null } });
-    mount(
-      <Provider store={store}>
-        <WorkingArea />
-      </Provider>,
-    );
-    const action = store.getActions();
-    expect(action).toEqual({ type: 'TICK' });
-    // expect(action).toEqual({ type: 'INIT', payload: { width: 4, height: 4 } });
+  it('Working area contains a EngineGrid', () => {
+    expect(workingArea.find('EngineGrid').exists()).toEqual(true);
   });
-  it('should call init and tick', (success) => {
-    const store = mockStore({ currentAction: { action: null } });
-    shallow(
-      (
-        <Provider store={store}>
-          <WorkingArea username="pepe" gameId="5cffa5985fc42e0ea1f8a420" />
-        </Provider>), { disableLifecycleMethods: true },
-    );
-    setTimeout(() => {
-      // expect(store.getActions()).toEqual([{ type: 'INIT', payload:
-      // { width: 4, height: 4 } }, { type: 'TICK' }]);
-      expect(store.getActions()[0]).toEqual({ type: 'TICK' });
-      success();
-    }, 3005);
-  }); */
+  it('Working area contains a information', () => {
+    expect(workingArea.find('div').at(1).hasClass('earnings-information')).toEqual(true);
+  });
 });
