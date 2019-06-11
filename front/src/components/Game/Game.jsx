d63be74bc1b729d79ec882e035534acc8cd8e2ea
Facundo Videla <facundokhalil@gmail.com>
2019-06-11 19:04:36 -0300

    Issue 86 create button save game (#87)
    * Se agrego react router
    
    * Se creo la pantalla de login
    
    * hice cagadas
    
    * Se termino la pantalla de login
    
    * arregle la cagada
    
    * Se creo la pantalla del perfil del usuario
    
    * La pantalla de login ya redirige. Se pueden traer los games en el userProfile
    
    * Ya se puede crear un juego desde el front
    
    * Se agregaron los servicios para pegarle al back
    
    * Se arreglo la fecha y cantidad de maquinas al mostrar en el perfil
    
    * tests medios pelo de la vista. Falta mockear service y testear la logica
    
    * Boton para volver al perfil del usuario

--- front/src/components/Game/Game.jsx
+++ front/src/components/Game/Game.jsx
@@ -3,17 +3,38 @@ import ToolboxMenu from '../ToolBox/ToolboxMenu';
 import WorkingArea from '../WorkingArea/WorkingArea';
 import Information from '../Information/Information';
 
-const Game = () => (
-  <div className="grid-container">
-    <div className="header-container">
-      <h1>Revolución Industrial</h1>
-    </div>
-    <div className="body-container">
-      <ToolboxMenu />
-      <WorkingArea />
-      <Information />
-    </div>
-  </div>
-);
+export default class Game extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      username: this.props.match.params.user,
+      gameId: this.props.match.params.gameId,
+    };
+  }
 
-export default Game;
+  returnToUser() {
+    this.props.history.goBack();
+  }
+
+  render() {
+    return (
+      <div className="grid-container">
+        <div className="header-container">
+          <button
+            type="button"
+            className="games-button btn btn-secondary col-1"
+            onClick={() => this.returnToUser()}
+          >
+            ◀
+          </button>
+          <h1 className="header-title col-5 offset-md-3">Revolución Industrial</h1>
+        </div>
+        <div className="body-container">
+          <ToolboxMenu />
+          <WorkingArea username={this.state.username} gameId={this.state.gameId} />
+          <Information />
+        </div>
+      </div>
+    );
+  }
+}
