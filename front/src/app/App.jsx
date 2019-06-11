53ab13ad5b5275d1e28cb658d97e9450c7fb0ff5
Leandro <leandro.jdl@gmail.com>
2019-06-10 21:44:09 -0300

    Fix basename

--- front/src/app/App.jsx
+++ front/src/app/App.jsx
@@ -5,10 +5,15 @@ import Login from '../components/User/Login';
 import Game from '../components/Game/Game';
 import UserProfile from '../components/User/UserProfile';
 
+// Forma cabeza de definir el basename.
+// Idealmente habría que utilizar ENV vars,
+// pero no encontramos manera de poder setearlas en github pages.
+const basename = window.location.hostname === 'practicads.github.io' ? 'pdes-tp-anonymous' : '';
+
 class App extends Component {
   render() {
     return (
-      <Router basename="pdes-tp-anonymous">
+      <Router basename={basename}>
         <Route exact path="/" component={Login} />
         <Route exact path="/:user" component={UserProfile} />
         <Route exact path="/:user/:gameId" component={Game} />
