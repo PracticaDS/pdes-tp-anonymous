4a19dcaa3b27a41e172d5f26f3d84e494e6c4e02
Leandro <leandro.jdl@gmail.com>
2019-06-17 20:18:47 -0300

    Primer versión del dockerfile para el front
    Fixing dockerfile
    
    
    min fix

--- front/src/app/App.jsx
+++ front/src/app/App.jsx
@@ -5,15 +5,10 @@ import Login from '../components/User/Login';
 import Game from '../components/Game/Game';
 import UserProfile from '../components/User/UserProfile';
 
-// Forma cabeza de definir el basename.
-// Idealmente habría que utilizar ENV vars,
-// pero no encontramos manera de poder setearlas en github pages.
-const basename = window.location.hostname === 'practicads.github.io' ? 'pdes-tp-anonymous' : '';
-
 class App extends Component {
   render() {
     return (
-      <Router basename={basename}>
+      <Router>
         <Route exact path="/" component={Login} />
         <Route exact path="/:user" component={UserProfile} />
         <Route exact path="/:user/:gameId" component={Game} />
