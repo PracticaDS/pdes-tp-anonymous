9e64d1794c5d51bfc1f790bb2c84e823b8218e97
Leandro Di Lorenzo <3334920+leandrojdl@users.noreply.github.com>
2019-06-17 21:29:42 -0300

    Issue 88 docker front (#93)
    * Primer versión del dockerfile para el front
    
    
    Fixing dockerfile
    
    
    min fix
    
    * Remove unnecesary file & service with env vars
    
    * Achicando el docker

--- front/src/app/Service.jsx
+++ front/src/app/Service.jsx
@@ -2,37 +2,47 @@ import axios from 'axios';
 import reducer from '../reducers/toolboxReducer';
 import actions from '../actions/toolboxActions';
 
-const server = 'http://localhost:8080';
+const api = {
+  protocol: process.env.REACT_APP_API_PROTOCOL || 'http',
+  host: process.env.REACT_APP_API_HOST || 'localhost',
+  port: process.env.REACT_APP_API_PORT || 8080,
+};
+api.url = `${api.protocol}://${api.host}:${api.port}`;
 
-const getUserAndCreate = user => axios.get(`${server}/${user}`)
-  .then(res => res.data)
-  .catch(error => console.info(error));
+const service = {
+  getUserAndCreate: user => axios
+    .get(`${api.url}/${user}`)
+    .then(res => res.data)
+    .catch(error => console.info(error)),
 
-const deleteGame = (user, id) => axios.delete(`${server}/${user}/games/${id}`)
-  .then(res => res.data)
-  .catch(error => console.info(error));
+  deleteGame: (user, id) => axios
+    .delete(`${api.url}/${user}/games/${id}`)
+    .then(res => res.data)
+    .catch(error => console.info(error)),
 
-const createGame = (user, gameName) => {
-  const state = reducer(undefined, actions.init(4, 4));
-  const date = new Date();
-  const dateString = `${date.getDate()}/${(date.getMonth() + 1)}/${date.getFullYear()}`;
+  createGame: (user, gameName) => {
+    const state = reducer(undefined, actions.init(4, 4));
+    const date = new Date();
+    const dateString = `${date.getDate()}/${(date.getMonth() + 1)}/${date.getFullYear()}`;
+    const newGame = {
+      name: gameName,
+      date: dateString,
+      state,
+    };
 
-  const newGame = {
-    name: gameName,
-    date: dateString,
-    state,
-  };
-  return axios.post(`${server}/${user}/games`, newGame)
-    .then(savedUser => savedUser)
-    .catch(error => console.info(error));
+    return axios
+      .post(`${api.url}/${user}/games`, newGame)
+      .then(savedUser => savedUser)
+      .catch(error => console.info(error));
+  },
+
+  getGame: (user, gameId) => axios.get(`${api.url}/${user}/games/${gameId}`)
+    .then(res => res.data)
+    .catch(error => console.info(error)),
+
+  updateGame: (user, gameId, state) => axios.put(`${api.url}/${user}/games/${gameId}`, state)
+    .then(res => res.data)
+    .catch(error => console.info(error)),
 };
 
-const getGame = (user, gameId) => axios.get(`${server}/${user}/games/${gameId}`)
-  .then(res => res.data)
-  .catch(error => console.info(error));
-
-const updateGame = (user, gameId, state) => axios.put(`${server}/${user}/games/${gameId}`, state)
-  .then(res => res.data)
-  .catch(error => console.info(error));
-
-export default { getUserAndCreate, deleteGame, createGame, updateGame, getGame };
+export default service;
