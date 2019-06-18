98c92f0978d533e1039c8bc6c3b56b76daf15bc2
Leandro Di Lorenzo <3334920+leandrojdl@users.noreply.github.com>
2019-06-18 20:21:24 -0300

    Building Docker Containers from travis (#96)

---
+++ .travis/docker_and_deploy.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+if [ "${TRAVIS_BRANCH}" = "master" ] && [ "${TRAVIS_PULL_REQUEST}" = "false" ]; then
+    echo "*** Building Docker Containers ***"
+    docker build -t pdesanonymous/api .docker/back/
+    docker build -t pdesanonymous/web .docker/front/
+
+    echo "*** Pushing Docker Containers ***"
+    echo "$DOCKER_PASSWORD" | docker login -u "$DOCKER_USERNAME" --password-stdin
+    docker push pdesanonymous/api:latest
+    docker push pdesanonymous/web:latest
+
+    echo "*** Deploying on Server ***"
+    echo "$SERVER_IP ecdsa-sha2-nistp256 $SERVER_KEY" >> ~/.ssh/known_hosts
+    ssh $SERVER_USERNAME@$SERVER_IP "/home/$SERVER_USERNAME/pdes-tp-anonymous-deploy.sh"
+else
+    echo "Not deploying, since this branch isn't master or it's a Pull Request."
+fi
