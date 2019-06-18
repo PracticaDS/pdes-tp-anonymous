2b1f308983640302f8d04e91ae8a8e223849aa58
Leandro <leandro.jdl@gmail.com>
2019-06-18 19:29:07 -0300

    Building Docker Containers from travis

--- .travis/deploy_back.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-if [ "${TRAVIS_BRANCH}" = "master" ] && [ "${TRAVIS_PULL_REQUEST}" = "false" ]; then
-    echo "*** Deploying Back ***"
-    echo "162.243.162.94 ecdsa-sha2-nistp256 $SERVER_KEY" >> ~/.ssh/known_hosts
-    ssh forge@162.243.162.94 '/home/forge/pdes-tp-anonymous-deploy.sh'
-else
-    echo "Not deploying, since this branch isn't master."
-fi
